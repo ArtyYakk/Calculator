1a0a503169f20be670da02209a14f856cfde2b89
iakon <iakonien@gmail.com>
2022-06-18 00:39:30 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String usr = sc.nextLine();
+        try{
+            String[] musr = usr.split(" ");
+            switch (musr[1]){
+                case ("+"):
+                    System.out.println(Double.parseDouble(musr[0]) + Double.parseDouble(musr[2]));
+                    break;
+                case ("-"):
+                    System.out.println(Double.parseDouble(musr[0]) - Double.parseDouble(musr[2]));
+                    break;
+                case ("*"):
+                    System.out.println(Double.parseDouble(musr[0]) * Double.parseDouble(musr[2]));
+                    break;
+                case ("/"):
+                    if(Double.parseDouble(musr[2]) == 0){
+                        throw new Exception("Error! Division by zero");
+                        //break;
+                    }
+                    System.out.println(Double.parseDouble(musr[0]) / Double.parseDouble(musr[2]));
+                    break;
+                default:
+                    throw new Exception("Operation Error!");
+            }
+        }
+        catch (NumberFormatException e){
+            System.out.println("Error! Not a number");
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+
+
+    }
+}
