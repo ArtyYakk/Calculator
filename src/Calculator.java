889011e47fbb07212b7c22be7a3f94880da3cae7
iakon <iakonien@gmail.com>
2022-06-18 01:16:23 +0300

    Порядок исключений

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,38 +3,39 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String usr = sc.nextLine();
-        try{
-            String[] musr = usr.split(" ");
-            switch (musr[1]){
-                case ("+"):
-                    System.out.println(Double.parseDouble(musr[0]) + Double.parseDouble(musr[2]));
-                    break;
-                case ("-"):
-                    System.out.println(Double.parseDouble(musr[0]) - Double.parseDouble(musr[2]));
-                    break;
-                case ("*"):
-                    System.out.println(Double.parseDouble(musr[0]) * Double.parseDouble(musr[2]));
-                    break;
-                case ("/"):
-                    if(Double.parseDouble(musr[2]) == 0){
-                        throw new Exception("Error! Division by zero");
-                        //break;
-                    }
-                    System.out.println(Double.parseDouble(musr[0]) / Double.parseDouble(musr[2]));
-                    break;
-                default:
-                    throw new Exception("Operation Error!");
+        while(true){
+            String usr = sc.nextLine();
+            try{
+                String[] musr = usr.split(" ");
+                double a = Double.parseDouble(musr[0]);
+                double b = Double.parseDouble(musr[2]);
+                switch (musr[1]){
+                    case ("+"):
+                        System.out.println(a + b);
+                        break;
+                    case ("-"):
+                        System.out.println(a - b);
+                        break;
+                    case ("*"):
+                        System.out.println(a * b);
+                        break;
+                    case ("/"):
+                        if(Double.parseDouble(musr[2]) == 0){
+                            throw new Exception("Error! Division by zero");
+                            //break;
+                        }
+                        System.out.println(a / b);
+                        break;
+                    default:
+                        throw new Exception("Operation Error!");
+                }
+            }
+            catch (NumberFormatException e){
+                System.out.println("Error! Not number");
+            }
+            catch(Exception ex){
+                System.out.println(ex.getMessage());
             }
         }
-        catch (NumberFormatException e){
-            System.out.println("Error! Not number");
-        }
-        catch(Exception ex){
-            System.out.println(ex.getMessage());
-        }
-
-
-
     }
 }
