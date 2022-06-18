abdccd97c301263c508025dd83f7aaeef86e5cb7
iakon <iakonien@gmail.com>
2022-06-18 14:42:13 +0300

    Сделал "современный" switch

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,25 +11,18 @@ public class Calculator {
                 String[] musr = usr.split(" ");
                 double a = Double.parseDouble(musr[0]);
                 double b = Double.parseDouble(musr[2]);
-                switch (musr[1]){
-                    case ("+"):
-                        System.out.println(a + b);
-                        break;
-                    case ("-"):
-                        System.out.println(a - b);
-                        break;
-                    case ("*"):
-                        System.out.println(a * b);
-                        break;
-                    case ("/"):
-                        if(Double.parseDouble(musr[2]) == 0){
+                switch (musr[1]) {
+                    case ("+") -> System.out.println(a + b);
+                    case ("-") -> System.out.println(a - b);
+                    case ("*") -> System.out.println(a * b);
+                    case ("/") -> {
+                        if (Double.parseDouble(musr[2]) == 0) {
                             throw new Exception("Error! Division by zero");
                             //break;
                         }
                         System.out.println(a / b);
-                        break;
-                    default:
-                        throw new Exception("Operation Error!");
+                    }
+                    default -> throw new Exception("Operation Error!");
                 }
             }
             catch (FileNotFoundException ex){
