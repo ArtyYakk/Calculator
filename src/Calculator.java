786dbaacf6493f7a5bddd5c74c1aee0cf0ad4e30
iakon <iakonien@gmail.com>
2022-06-18 18:17:55 +0300

    Теперь можно вычислять независимо от количества строк

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,33 +8,36 @@ public class Calculator {
                 FileOutputStream fos = new FileOutputStream("C:\\Users\\iakon\\IdeaProjects\\Calculator\\output.txt");
                 PrintStream printStream = new PrintStream(fos);
                 Scanner sc = new Scanner(file);
-                String usr = sc.nextLine();
-                String[] musr = usr.split(" ");
-                double a = Double.parseDouble(musr[0]);
-                double b = Double.parseDouble(musr[2]);
-                switch (musr[1]) {
-                    case ("+") -> {
-                        System.out.println(a + b);
-                        printStream.println(a + b);
-                    }
-                    case ("-") -> {
-                        System.out.println(a - b);
-                        printStream.println(a - b);
-                    }
-                    case ("*") -> {
-                        System.out.println(a * b);
-                        printStream.println(a * b);
-                    }
-                    case ("/") -> {
-                        if (Double.parseDouble(musr[2]) == 0) {
-                            throw new Exception("Error! Division by zero");
-                            //break;
+                while(sc.hasNextLine()){
+                    String usr = sc.nextLine();
+                    String[] musr = usr.split(" ");
+                    double a = Double.parseDouble(musr[0]);
+                    double b = Double.parseDouble(musr[2]);
+                    switch (musr[1]) {
+                        case ("+") -> {
+                            System.out.println(a + " + " + b + " = " + (a + b));
+                            printStream.println(a + " + " + b + " = " + (a + b));
                         }
-                        System.out.println(a / b);
-                        printStream.println(a / b);
+                        case ("-") -> {
+                            System.out.println(a + " - " + b + " = " + (a - b));
+                            printStream.println(a + " - " + b + " = " + (a - b));
+                        }
+                        case ("*") -> {
+                            System.out.println(a + " * " + b + " = " + (a * b));
+                            printStream.println(a + " * " + b + " = " + (a * b));
+                        }
+                        case ("/") -> {
+                            if (Double.parseDouble(musr[2]) == 0) {
+                                throw new Exception("Error! Division by zero");
+                                //break;
+                            }
+                            System.out.println(a + " / " + b + " = " + (a / b));
+                            printStream.println(a + " / " + b + " = " + (a / b));
+                        }
+                        default -> throw new Exception("Operation Error!");
                     }
-                    default -> throw new Exception("Operation Error!");
                 }
+
             }
 
             catch (FileNotFoundException ex){
